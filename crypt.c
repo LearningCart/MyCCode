a7424350800ee30d6d9647618adc964d55d81267
LearningCart <gandhijatink@gmail.com>
2020-02-20 10:42:58 +0000

    Removed unused function.

--- crypt.c
+++ crypt.c
@@ -126,33 +126,6 @@ char *CryptEncode(const char *msg, int shift)
 	return EncodedMsg;
 }
 
-int toString(char* a) {
-  int c, sign, offset, n;
-
-  if (a[0] == '-') {  // Handle negative integers
-    sign = -1;
-  }
-
-  if (sign == -1) {  // Set starting position to convert
-    offset = 1;
-  }
-  else {
-    offset = 0;
-  }
-
-  n = 0;
-
-  for (c = offset; a[c] != '\0'; c++) {
-    n = n * 10 + a[c] - '0';
-  }
-
-  if (sign == -1) {
-    n = -n;
-  }
-
-  return n;
-}
-
 char ReverseLookup(char *code)
 {
 	int index;
