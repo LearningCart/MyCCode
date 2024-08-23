06b3dea7bdee45ad7efcfea4ed2e09d73b175348
gandhijatink <61272952+LearningCart@users.noreply.github.com>
2024-08-23 08:56:53 +0100

    Script to create 100 copies of a file
    Commit: Rectified filename

--- copy.sh
+++ copy.sh
@@ -7,7 +7,7 @@ inputfile="test.bin"
 
 for i in `seq 1 $max`
 do
-	echo -n "Copying File test.bin to : $name$counter.bin"
+	echo -n "Copying File $inputfile to : $name$counter.bin"
 	cp $inputfile "$name$counter.bin"
 	echo "      DONE"
 	counter=`expr $counter + 1`
